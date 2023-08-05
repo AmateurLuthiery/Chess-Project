0856d8c5891dcfc77c1c58b81725545aac6c9567
AmateurLuthiery <dick.p.welch@gmail.com>
2023-08-05 17:15:57 -0400

    Added encoding and making a list of action/next states

--- chess.py
+++ chess.py
@@ -1,5 +1,7 @@
 from collections import Counter
 import numpy as np
+import copy
+
 
 start_board=[
     ["r","n","b","q","k","b","n","r"],
@@ -34,9 +36,9 @@ pawn_move:  This one returns the list of allowable pawn moves.
 
 At some point, we will need to add in castling, but for now it is not implemented
 
-board_number:  encodes the board into a 64 character hexadecimal
+encode_board:  encodes the board into a 64 character base-13 number
 
-board_complement:  encodes a left/right flip of the board as a 64 character hexadecimal
+board_complement:  encodes a left/right flip of the board as a 64 character base 13 number
 
 
 
@@ -314,7 +316,161 @@ def pawn_move(board, position):
         return
     return
         
+#converts a board to a pair of unique integer number
+# the first number is the board
+# the second number is it's right/left flip complement
+#board_value and comp_value work
+def encode_board(board):
+    board_value=""
+    comp_value=""
+    for r in range(8):
+        temp_comp=""
+        for c in range(8):
+            match board[r][c]:
+                case " ": 
+                    val="0"
+                case "p":
+                    val="1"
+                case "n": 
+                    val="2"
+                case "b":
+                    val="3"
+                case "r": 
+                    val="4"
+                case "q":
+                    val="5"
+                case "k": 
+                    val="6"
+                case "P":
+                    val="7"
+                case "N": 
+                    val="8"
+                case "B":
+                    val="9"
+                case "R": 
+                    val="A"
+                case "Q":
+                    val="B"
+                case "K": 
+                    val="C" 
+            board_value=board_value+val
+            temp_comp=val+temp_comp
+        comp_value=comp_value+temp_comp
+        temp_comp=""
+    board_int=int(board_value,13)
+    comp_int=int(comp_value,13)
+    return (board_int,comp_int)
+
+#decode works
+def decode_board(boardcode):
+    decoded=[]
+    for l in range(64):
+        LSD=boardcode % 13
+        boardcode //= 13
+        match LSD:
+            case 0: 
+                decoded.append(" ")
+            case 1:
+                decoded.append("p")
+            case 2:
+                decoded.append("n")
+            case 3:
+                decoded.append("b")
+            case 4:
+                decoded.append("r")
+            case 5:
+                decoded.append("q")
+            case 6:
+                decoded.append("k")
+            case 7:
+                decoded.append("P")
+            case 8:
+                decoded.append("N")
+            case 9:
+                decoded.append("B")
+            case 10:
+                decoded.append("R")
+            case 11:
+                decoded.append("Q")
+            case 12:
+                decoded.append("K")
+    decoded.reverse()
+    return np.reshape(decoded,(8,8))
+
+#hashcode to store the boards in a table indexed by a hash.  Initial hash is the 
+#number of blank spaces on the board, which should monotonically increase as game
+#progresses.
+
+def hashcode(board):
+    
+    # Python3 code to demonstrate
+# Matrix elements Frequencies Counter
+
+    res=dict()
+    a=[]
+    for i in board:
+        a.extend(i)
+    b=set(a)
+    for i in b:
+        res[i]=a.count(i)
+ 
+    return res
+
+def make_move(board, curr_r, curr_c, next_r, next_c):
+    new_board = copy.deepcopy(board)
+    temp=board[curr_r][curr_c]
+    new_board[next_r][next_c]=temp
+    new_board[curr_r][curr_c]=" "
+    return new_board
+
+#black_moves generates a list of actions and next states from a given board position
+#currently does not include en passant and castling
+
+def black_moves(board):
+    result=[]
+    for r in range(8):
+        for c in range(8):
+            if is_black(board[r][c]):
+                match board[r][c]:
+                    case "p":
+                        moves=pawn_move(board,(r,c))
+                    case "r":
+                        moves=rook_move(board,(r,c))
+                    case "b":
+                        moves=bishop_move(board,(r,c))
+                    case "n":
+                        moves=knight_move(board,(r,c))
+                    case "q":
+                        moves=queen_move(board,(r,c))
+                    case "k":
+                        moves=king_move(board, (r,c))
+                if len(moves)>0:
+                    for i in moves:
+                        result.append((i,make_move(board,r,c,i[0],i[1])))
+    return result
+
+def white_moves(board):
+    result=[]
+    for r in range(8):
+        for c in range(8):
+            if is_white(board[r][c]):
+                match board[r][c]:
+                    case "P":
+                        moves=pawn_move(board,(r,c))
+                    case "R":
+                        moves=rook_move(board,(r,c))
+                    case "B":
+                        moves=bishop_move(board,(r,c))
+                    case "N":
+                        moves=knight_move(board,(r,c))
+                    case "Q":
+                        moves=queen_move(board,(r,c))
+                    case "K":
+                        moves=king_move(board, (r,c))
+                if len(moves)>0:
+                    for i in moves:
+                        result.append((i,make_move(board,r,c,i[0],i[1])))
+    return result
 
 #main loop
-test = pawn_move(start_board,(6,3))
-print (test)
+print(white_moves(start_board))
